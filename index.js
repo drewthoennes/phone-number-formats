80c0f17eb2e369a3ab5483ec8464ba65896b1354
drewthoennes <dthoenne@purdue.edu>
2020-05-07 00:14:19 -0400

    Removes unnecessary dependency

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const replaceAt = require('replace-at')
 const privateMethods = {
   convertSingleChar(char) {
     const keyboard = {
@@ -48,6 +47,30 @@ let types = {
       number: 'X',
       areaCode: 'Y',
     }
+  }
+}
+
+const replaceAt = (string, index, replacement, options) => {
+  if (typeof(string) !== 'string') {
+    throw new TypeError('Expected a string but got: ' + typeof(string));
+  } else if (typeof(index) !== 'number') {
+    throw new TypeError('Expected a number but got: ' + typeof(index));
+  } else if(typeof(replacement) !== 'string') {
+    throw new TypeError('Expected a string but got: ' + typeof(replacement));
+  }
+
+  if (index > string.length) {
+    throw new RangeError('Index is out of range');
+  }
+
+  options = Object.assign({
+    inline: false
+  }, options);
+
+  if (options['inline']) {
+    return string.substring(0, index) + replacement + string.substring(index + replacement.length);
+  } else {
+    return string.substring(0, index) + replacement + string.substring(index + 1);
   }
 }
 
